03320acb9ebb63ae13fa92d710dfb2904a5e971a
SuhailDTU <suhail230014@gmail.com>
2022-01-10 12:31:46 +0100

    second commit

---
+++ hdmitest/Debug/src/timer_ps/timer_ps.d
@@ -0,0 +1,49 @@
+src/timer_ps/timer_ps.o src/timer_ps/timer_ps.o: \
+ ../src/timer_ps/timer_ps.c ../src/timer_ps/timer_ps.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../hdmitest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/timer_ps/timer_ps.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../hdmitest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
